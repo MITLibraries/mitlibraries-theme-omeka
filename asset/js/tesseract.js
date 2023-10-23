9caf5b75c282f87bda137703ae480cc982e42a38
Matthew Bernhardt <mjbernha@mit.edu>
2023-10-23 19:04:43 -0400

    Integrate SmartMenus with two navigation locations
    ** Why are these changes being introduced:
    
    * With the previous commit having added SmartMenus as an external
      dependency, we now need to update the theme to actually use that
      library.
    
    ** Relevant ticket(s):
    
    * https://mitlibraries.atlassian.net/browse/post-52
    
    ** How does this address that need:
    
    * The theme configuration file defines a "nav_style" customization
      option, which allows site builders to choose whether the navigation
      should appear as a navbar or along the left side of the content column
    * There are two template partials which correspond to these two layout
      choices.
    * The main layout.phtml file now includes calls to the javascript and
      stylesheet files. It also loads the nav_style choice and defines some
      classes which allow our style library to include the appropriate
      layout classes.
    * If the vertical menu is chosen, the addition of .nav-vertical to the
      body element causes the entire layout to get wider by about a third,
      giving space for both the menu and the content column.
    
    ** Document any side effects to this change:
    
    * We are disabling the CSS transitions for the navigation menu, as that
      proved distracting in practice.
    * Some padding is removed from the page-menu class now that a menu is
      being placed.
    * I'm a little worried about the unused nav-horizontal class, but there
      may yet be a use for this class name down the road.

---
+++ asset/js/tesseract.js
@@ -0,0 +1,8 @@
+$(function() {
+    $('ul.navigation').addClass('sm sm-mint');
+    $('body.nav-vertical ul.navigation').addClass('sm-vertical');
+    $('ul.navigation').smartmenus({
+        subMenusSubOffsetX: 6,
+        subMenusSubOffsetY: -8
+    });
+});
