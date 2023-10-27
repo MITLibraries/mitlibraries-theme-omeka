cd7ef8363ff19943e371b819589002666858496f
Matthew Bernhardt <mjbernha@mit.edu>
2023-10-27 18:54:58 -0400

    Integrate smartmenus with the rest of Omeka theme

---
+++ asset/js/tesseract.js
@@ -0,0 +1,7 @@
+$(function() {
+    $('#navbar-container ul.navigation').addClass('sm sm-mint');
+    $('#navbar-container ul.navigation').smartmenus({
+        subMenusSubOffsetX: 6,
+        subMenusSubOffsetY: -8
+    });
+});
