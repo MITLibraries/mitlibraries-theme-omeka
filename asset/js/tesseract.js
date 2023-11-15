739c7d8170b76b74d0a38867ecf227f4305c085e
Matthew Bernhardt <mjbernha@mit.edu>
2023-11-15 13:58:49 -0500

    Bugfix and intentional side effects
    * My initial implementation of SmartMenus looked simply for the
      ul.navigation markup in the DOM, but we've since realized that
      Omeka uses that markup in more places than the navigation menus.
      As a result, I've made that javascript more specific, adding
      the ID value of the containing element (which we added as part
      of the navigation-* partials - so it should now be unique).
    
    * Bump theme version to 0.4

--- asset/js/tesseract.js
+++ asset/js/tesseract.js
@@ -1,7 +1,10 @@
 $(function() {
-    $('ul.navigation').addClass('sm sm-mint');
-    $('body.nav-vertical ul.navigation').addClass('sm-vertical');
-    $('ul.navigation').smartmenus({
+    // Only the vertical configuration gets this class.
+    $('body.nav-vertical #navbar-container ul.navigation').addClass('sm-vertical');
+    // All SmartMenus get these classes.
+    $('#navbar-container ul.navigation').addClass('sm sm-mint');
+    // All SmartMenus get called.
+    $('#navbar-container ul.navigation').smartmenus({
         subMenusSubOffsetX: 6,
         subMenusSubOffsetY: -8
     });
