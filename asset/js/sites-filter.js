8fb13c7d06f9a888785f255a5a71d46746e0d7f2
Matthew Bernhardt <mjbernha@mit.edu>
2024-03-05 15:29:30 -0500

    Refactor landing filter to be keyboard accessible
    ** Why are these changes being introduced:
    
    The filterable display of exhibits on the platform landing page is not,
    as it turns out, keyboard accessible.
    
    In addition to this problem, the feature as implemented received a set
    of critiques during accessibility review, which should all be achievable
    with a refactor.
    
    ** Relevant ticket(s):
    
    * https://mitlibraries.atlassian.net/browse/post-182
    
    ** How does this address that need:
    
    Changes include:
    * Switching the filter inputs from radio elements to buttons with a
      radio role.
    * Adding a live panel (visually hidden) to announce the total number of
      currently visible exhibits, repeating the selected category.
    * Adding some javascript functions to update the filter panel, and
      maintain the state of the filter buttons. This includes setting the
      aria-pressed attribute as appropriate.
    * Updating the markup of the filtered exhibits, including making them a
      list. Also, the DOM order of the exhibits is now more sensible, with
      the title before the image. We preserve the visual layout using an
      order rule.
    * Adding a legend element to the filter fieldset, with appropriate CSS
      updates.
    * The inline javascript in the sites-filter.phtml partial is now more
      minimal, with only a call into the set of functions. The assignment of
      the event listeners now happens in the javascript file, which should
      be more robust to slow API responses.
    
    ** Document any side effects to this change:
    
    * We tweak the visual layout, making exhibits take up 1/3 of the page
      rather than 30% width.
    * There is a bug in some screen reader / browser combinations, in that
      VoiceOver with Firefox still reads the title after the image alt
      attribute. Other combinations tested are correct, however.
    * Legend styling can be finicky, as they do not respond well to many
      display rules. I was able to get it to mostly behave using float,
      which feels icky but has an acceptable outcome.

--- asset/js/sites-filter.js
+++ asset/js/sites-filter.js
@@ -1,17 +1,23 @@
 const pluck = property => element => element[property];
 
 var filter_values = new Array();
+var visible_sites = 0;
 
 function ApplyFilter( value ) {
     const grid = document.getElementById("site-grid").getElementsByClassName("site-wrapper");
+    const live = document.getElementById("filter-live");
     $(grid).each(function() {
         let subjects = $(this).data("subject").split(" ");
         if ( subjects.includes(value) ) {
             $(this).removeClass('hidden');
+            $(this).addClass('visible');
         } else {
             $(this).addClass('hidden');
+            $(this).removeClass('visible');
         }
-    })
+    });
+    visible_sites = document.getElementsByClassName("site-wrapper visible").length;
+    live.innerHTML = `There are ${visible_sites} exhibits visible in the ${value} category.`;
 }
 
 function BuildFilter( value ) {
@@ -28,7 +34,7 @@ function RenderFilter() {
     const filter = document.getElementById("site-filter");
     filter_values.sort();
     $(filter_values).each(function(index, value) {
-        let markup = `<input id="${Hyphenate(value)}" type="radio" name="site-filter-value" value="${Hyphenate(value)}"><label for="${Hyphenate(value)}" class="btn button-secondary">${value}</label>`
+        let markup = `<button role="radio" class="filter-button" value="${Hyphenate(value)}">${value}</button>`;
         filter.innerHTML += markup;
     });
 }
@@ -43,13 +49,44 @@ function RenderSite( site ) {
         img = `<img src="${site["thumbnail_display_urls"]["large"]}" alt="${site["dcterms:description"][0]["@value"]}">`
     }
     let markup =
-        `<div class="site-wrapper" data-subject="${subjectlist}">
+        `<li class="site-wrapper" data-subject="${subjectlist}">
           <a href="${site["bibo:uri"][0]["@id"]}" class="site">
-            ${img}
             <span class="site-title">${site["o:title"]}</span>
+            ${img}
           </a>
-        </div>`;
+        </li>`;
     grid.innerHTML += markup;
+};
+
+function ResetFilters() {
+    let filters = document.getElementsByClassName("filter-button");
+
+    for (let i = 0; i < filters.length; i++) {
+        filters[i].setAttribute('aria-pressed', 'false');
+        $(filters[i]).removeClass('pressed');
+    }
+}
+
+function UpdateFilter( e ) {
+    console.log(e);
+    e.setAttribute('aria-pressed', 'true');
+    $(e).toggleClass('pressed');
+}
+
+function ActivateFilter() {
+    let filters = document.getElementsByClassName("filter-button");
+
+    for (let i = 0; i < filters.length; i++) {
+        filters[i].addEventListener(
+            "click",
+            (event) => {
+                ResetFilters();
+                UpdateFilter(event.target);
+                ApplyFilter(event.target.value);
+            },
+            false,
+        );
+    }
 };
 
 function LoadItemSet( id ) {
@@ -59,5 +96,7 @@ function LoadItemSet( id ) {
         data.map(RenderSite);
     }).done(function() {
         RenderFilter();
+        ActivateFilter();
+        ApplyFilter('All');
     });
 }
