6dced22f965124079b82933eaff426b9807b335d
Matthew Bernhardt <mjbernha@mit.edu>
2024-03-18 17:26:41 -0400

    Following maintainers recommended SM2 instatiation

--- asset/js/tesseract.js
+++ asset/js/tesseract.js
@@ -6,12 +6,18 @@ docs/reference/navigation.md
 */
 
 $(function() {
-    $('#navbar-container ul.navigation').addClass('sm-nav sm-nav--left');
-    // const navbar1 = new SmartMenus(document.querySelector('#navbar-container'));
-    console.log('SmartMenus just fired.');
-    /*
+    // $('#navbar-container ul.navigation').addClass('sm-nav');
     // Only the vertical configuration gets this class.
-    $('body.nav-vertical #navbar-container ul.navigation').addClass('sm-vertical');
+    // $('body.nav-vertical #navbar-container ul.navigation').addClass('sm-vertical');
+    /*
+    const navbar1 = new SmartMenus(document.querySelector('#navbar-container'), {
+        selectorNav: '.navigation',
+        selectorSub: 'ul',
+        selectorItem: 'li',
+        selectorLink: 'a'
+    });
+    */
+    /*
     // All SmartMenus get these classes.
     $('#navbar-container ul.navigation').addClass('sm sm-mint');
     // All SmartMenus get called.
