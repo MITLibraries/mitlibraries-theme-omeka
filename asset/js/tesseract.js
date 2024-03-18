e63374f25d38c70cc13e25b5b28042551a7aa1c3
Matthew Bernhardt <mjbernha@mit.edu>
2024-03-18 10:01:52 -0400

    Removes SmartMenus
    This leaves in place some customization stylesheet modules, but those will need to be cleaned up separately.

--- asset/js/tesseract.js
+++ asset/js/tesseract.js
@@ -6,13 +6,5 @@ docs/reference/navigation.md
 */
 
 $(function() {
-    // Only the vertical configuration gets this class.
-    $('body.nav-vertical #navbar-container ul.navigation').addClass('sm-vertical');
-    // All SmartMenus get these classes.
-    $('#navbar-container ul.navigation').addClass('sm sm-mint');
-    // All SmartMenus get called.
-    $('#navbar-container ul.navigation').smartmenus({
-        subMenusSubOffsetX: 6,
-        subMenusSubOffsetY: -8
-    });
+    console.log('This is where the menu would initialize');
 });
