ce7274a221fece38051fe54ed4353e24d0438997
Matthew Bernhardt <mjbernha@mit.edu>
2024-04-08 16:40:52 -0400

    Apply UI fixes for iPhone quirks
    ** Why are these changes being introduced:
    
    * iPhones have a weird behavior where they zoom into form fields when
      they receive focus and have smaller than 16 px font size. The zoom is
      then not restored after removing focus.
    
    ** Relevant ticket(s):
    
    * https://mitlibraries.atlassian.net/browse/post-170
    
    ** How does this address that need:
    
    * This applies a Safari-specific viewport hack to prevent automatically
      zooming in. The viewport rule is applied only for iPhone (achieved via
      javascript).
    
    * As a defense in depth strategy, the two search forms (one-line form on
      all pages, and the advanced search form) is given a 16px font size on
      all interactive elements.
    
    The article detailing approach can be found at
    https://weblog.west-wind.com/posts/2023/Apr/17/Preventing-iOS-Safari-Textbox-Zooming
    
    ** Document any side effects to this change:
    
    * According to the article, either of these approaches should be
      sufficient alone - so it might be considered overkill to be using
      them both together. I'd rather be sure to not re-encounter this in a
      later sprint, however.

--- asset/js/tesseract.js
+++ asset/js/tesseract.js
@@ -6,6 +6,15 @@ docs/reference/navigation.md
 */
 
 $(function() {
+    // iPhones need a hack to prevent zooming into form fields.
+    // cite: https://weblog.west-wind.com/posts/2023/Apr/17/Preventing-iOS-Safari-Textbox-Zooming#selective-maximum-scale-for-safari-on-ios
+    if(navigator.userAgent.indexOf('iPhone') > -1 )
+    {
+        document
+          .querySelector("[name=viewport]")
+          .setAttribute("content","width=device-width, initial-scale=1, maximum-scale=1");
+    }
+
     // Only the vertical configuration gets this class.
     $('body.nav-vertical #navbar-container ul.navigation').addClass('sm-vertical');
     // All SmartMenus get these classes.
