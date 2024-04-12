4241fe74b9305e777c77a05dd065e852743a4346
Matthew Bernhardt <mjbernha@mit.edu>
2024-04-12 12:10:48 -0400

    Respond to code review feedback

--- helper/ResourceClasses.php
+++ helper/ResourceClasses.php
@@ -7,7 +7,9 @@ class ResourceClasses extends AbstractHelper
 {
 
     /**
-     * Returns a representation of resource classes in HTML.
+     * Returns a representation of resource classes in HTML. If this helper
+     * receives anything other than a resource, with a class, which itself has
+     * an ID, then it returns an empty string.
      *
      * @param object $resource The resource to add tags to.
      * @return string This is a set of HTML.
